aeb8657d74233af8f6de2c7ee00be11d71929dc0
Martin Koliba <mkoliba@users.noreply.github.com>
2021-07-28 17:02:13 +0200

    Removed scalebar plugin, upgraded dependencies

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,8 @@
-import scaleBarPlugin from '@mkoliba/phylocanvas-plugin-scalebar';
-
 import { createOnRedrawReRootTreePlugin } from './createOnRedrawReRootTree';
 import { createOnSelectPlugin } from './createOnSelect';
 import { createOnViewSubtreePlugin } from './createOnViewSubtree';
 
 export {
-  scaleBarPlugin,
   createOnSelectPlugin,
   createOnViewSubtreePlugin,
   createOnRedrawReRootTreePlugin,
